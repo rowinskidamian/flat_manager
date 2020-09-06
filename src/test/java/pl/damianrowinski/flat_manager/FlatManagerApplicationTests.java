865ad7ced53d2216a5f954c3375df4cbe6edb50e
Damian Rowiński <damian.rowinski@gmail.com>
2020-09-06 12:52:38 +0200

    initial commit

---
+++ src/test/java/pl/damianrowinski/flat_manager/FlatManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.damianrowinski.flat_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
