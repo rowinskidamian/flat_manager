56c269b5b1e1984fc4b2e7b86980ad0b6634bbd8
Damian Rowiński <damian.rowinski@gmail.com>
2020-09-20 16:39:54 +0200

    dodany wlasny banner wczytywania spring boota

--- src/main/java/pl/damianrowinski/flat_manager/services/AuthenticationService.java
+++ src/main/java/pl/damianrowinski/flat_manager/services/AuthenticationService.java
@@ -1,8 +1,41 @@
 package pl.damianrowinski.flat_manager.services;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.damianrowinski.flat_manager.config.Role;
+import pl.damianrowinski.flat_manager.domain.entities.User;
+import pl.damianrowinski.flat_manager.model.common.PersonalDetails;
 import pl.damianrowinski.flat_manager.model.dtos.UserAddDTO;
+import pl.damianrowinski.flat_manager.model.repositories.UserRepository;
 
-public interface AuthenticationService {
+import javax.transaction.Transactional;
 
-    void register(UserAddDTO userAddDTO);
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void register(UserAddDTO userAddDTO) {
+        User user = new User();
+        String encodedPassword = passwordEncoder.encode(userAddDTO.getPassword());
+        user.setLogin(userAddDTO.getLogin());
+        user.setPassword(encodedPassword);
+        user.setActive(true);
+        user.setRole(Role.USER.toString());
+
+        PersonalDetails personalDetails = user.getPersonalDetails();;
+        personalDetails.setEmail(userAddDTO.getEmail());
+
+        user.setPersonalDetails(personalDetails);
+
+        log.info("Próba zapisu użytkownika do bazy" + user);
+        userRepository.save(user);
+    }
 }
