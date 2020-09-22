df380f66559880a9e5e682083274b7b4ea90a6eb
Damian Rowiński <damian.rowinski@gmail.com>
2020-09-22 22:03:37 +0200

    usuniete zbedne importy

--- src/main/java/pl/damianrowinski/flat_manager/services/RoomService.java
+++ src/main/java/pl/damianrowinski/flat_manager/services/RoomService.java
@@ -2,20 +2,14 @@ package pl.damianrowinski.flat_manager.services;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-import pl.damianrowinski.flat_manager.domain.entities.Property;
 import pl.damianrowinski.flat_manager.domain.entities.Room;
-import pl.damianrowinski.flat_manager.domain.entities.Tenant;
 import pl.damianrowinski.flat_manager.model.dtos.RoomAddDTO;
-import pl.damianrowinski.flat_manager.model.repositories.PropertyRepository;
 import pl.damianrowinski.flat_manager.model.repositories.RoomRepository;
-import pl.damianrowinski.flat_manager.model.repositories.TenantRepository;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
